71edbc7f153b2a82bb8c17cd7890203c869345a4
twh <leo072060@163.com>
2024-10-09 14:33:19 +0800

    update

---
+++ build/CMakeFiles/ML.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ML.dir/src/main.cpp.o"
+  "CMakeFiles/ML.dir/src/main.cpp.o.d"
+  "CMakeFiles/ML.dir/src/managed.cpp.o"
+  "CMakeFiles/ML.dir/src/managed.cpp.o.d"
+  "ML"
+  "ML.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
